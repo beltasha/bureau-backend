15a6ef7720e58a3c2ce7c62f37da7cd54eafc7f2
Пользователь <semenckin.e@yandex.ru>
2019-09-27 22:40:33 +0300

    Added zaglushki

--- berua-backend/berua.API/Controllers/UsersController.cs
+++ berua-backend/berua.API/Controllers/UsersController.cs
@@ -5,13 +5,16 @@ using System.Threading.Tasks;
 using berau_backend.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using berua.API.Model;
 
 namespace berau_backend.Controllers
 {
     [Route("api/users")]
     [ApiController]
     public class UsersController : ControllerBase
-    {
+    {       
+        private List<Guid> _savedUsers = new List<Guid>();
+        
         [HttpPost]
         [Route("search")]
         public List<AccountDTO> Post([FromBody] string searchText)
@@ -29,15 +32,59 @@ namespace berau_backend.Controllers
         }
 
         [HttpPost]
-        public string Post([FromBody] Guid accountId)
+        [Route("posts")]
+        public IEnumerable<PostDTO> GetUserPosts([FromBody] string accountId)
+        {                     
+            var post1 = new PostDTO()
+            {
+                Id = new Guid().ToString(),
+                FirstName = "FirstNameTest",
+                LastName = "LastNameTest",
+                AvatarUrl = "https://sun9-12.userapi.com/c851016/v851016587/119cab/ai0uN_RKSXc.jpg?ava=1",
+                Text = "This is my Very big post text" +
+                "This is my Very big post text" +
+                "This is my Very big post text" +
+                "This is my Very big post text",
+                Tags = new string[5] { "красота", "здоровье", "секс", "шмекс", "хакатон" },
+                Likes = 2500,
+                PostUrl = "https://vk.com/mudakoff?w=wall-57846937_32571674",
+                Images = new string[2] { "https://sun9-12.userapi.com/c543105/v543105145/653c8/CO5L_Xf8-OI.jpg", "https://sun9-24.userapi.com/c543105/v543105158/4c299/Z3ge6QQsH0g.jpg" }
+            };
+
+            var postList = new List<PostDTO>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                var currentPost = post1;
+                currentPost.FirstName += i.ToString();
+                currentPost.LastName += i.ToString();
+                postList.Add(currentPost);
+            }
+
+            return postList;
+            
+        }
+
+        [HttpPost]
+        [Route("save")]
+        public IActionResult SaveUser([FromBody] Guid account)
         {
-            return "value";
+            _savedUsers.Add(account);
+            return Ok();
         }
 
         [HttpGet]
-        public string Get()
+        public IEnumerable<AccountDTO> GetSavedUsers()
         {
-            return "Something I'm not sure what";
+            var userList = new List<AccountDTO>();
+            foreach(var guid in _savedUsers)
+            {
+                var user = new AccountDTO();
+                user.Id = guid;
+                userList.Add(user);
+            }
+
+            return userList;
         }
     }
 }
