35676fabb33888028a7563b45ff7ce99b94f04f6
Daniel Zaitsev <danyzaitsev@gmail.com>
2019-09-27 19:36:54 +0300

    Users controller for frontend, model

---
+++ berua-backend/berua.API/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using berau_backend.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace berau_backend.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        [Route("search")]
+        public List<AccountDTO> Post([FromBody] string searchText)
+        {
+            return new List<AccountDTO>(){
+
+                new AccountDTO() {
+                    Id = new Guid(),
+                    Name = "Test Testerson",
+                    PhotoUrl = "https://sun9-52.userapi.com/c638624/v638624418/75575/ocYjIIcMfuY.jpg",
+                    AccountUrl = "https://vk.com/liz",
+                    Type = SocialNetworkType.vk
+                }   
+            };
+        }
+
+        [HttpPost]
+        public string Post([FromBody] Guid accountId)
+        {
+            return "value";
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Something I'm not sure what";
+        }
+    }
+}
---
+++ berua-backend/berua.API/Model/AccountDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace berau_backend.Model
+{
+    public class AccountDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public string PhotoUrl { get; set; }
+        public string AccountUrl { get; set; }
+        public SocialNetworkType Type { get; set; }
+
+    }
+}
