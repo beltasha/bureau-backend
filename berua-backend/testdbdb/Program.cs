e40b59f3fc9bf54af64c35ffcd17767b990a1b52
German Grachev <germangrach.94@yandex.ru>
2019-09-28 11:55:34 +0300

    Revert "Add translate"
    This reverts commit 3678780c51a037d9581cf6e302741e43eb78b219.

--- berua-backend/testdbdb/Program.cs
+++ berua-backend/testdbdb/Program.cs
@@ -1,5 +1,4 @@
-﻿using berua.BLL;
-using berua.BLL.Actions;
+﻿using berua.BLL.Actions;
 using berua.BLL.DTO;
 using System;
 using System.IO;
@@ -10,24 +9,16 @@ namespace testdbdb
     {
         static void Main(string[] args)
         {
-
-            string[] coll = new string[]
+            var user = new UserDTO
             {
-
-                @"The White House is reportedly considering de-listing Chinese companies from US stock exchanges as part of a broader effort to curb US investment in China.",
-                @"The US is also said to be looking at limiting the ability of government pension funds to participate in Chinese markets.",
-                @"The discussion comes amid a tense trade stand-off between the two countries.",
-                @"US stock exchanges dropped on the news, which Bloomberg first reported.",
-                @"Shares in Chinese companies listed in the US, which include big names such as Alibaba and JD.com, also fell sharply after the reports.",
-                @"Alibaba fell 5% while JD.com dropped more than 6 %."
+                Login = "Жанна",
+                Password = "11122233",
+                Phone = "9217777777"
             };
 
-            var res = TranslateText.TranslateEnToRu(coll);
-            foreach(var el in res)
-            {
-                Console.WriteLine(el);
-            }
-            Console.ReadLine();
+            UserAction.RegistrationUser(user);
+
+            UserAction.ValidateUser(user.Login, user.Password);
 
         }
     }
