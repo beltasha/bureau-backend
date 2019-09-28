999008c493b9dcf9424180977d43c6458eeecf9a
German Grachev <germangrach.94@yandex.ru>
2019-09-28 12:19:18 +0300

    add translate

---
+++ berua-backend/berua.BLL/TranslateText.cs
@@ -0,0 +1,44 @@
+﻿using Google.Cloud.Translation.V2;
+using System.Collections.Generic;
+
+namespace berua.BLL
+{
+    public static class TranslateText
+    {
+
+        private static readonly string ru = "ru";
+        private static readonly string en = "en";
+
+
+        public static ICollection<string> TranslateTextRuToEn(string[] couplets)
+        {
+            var result = new List<string>();
+            using (var client = TranslationClient.Create())
+            {
+                foreach(var elem in couplets)
+                {
+                    var response = client.TranslateText(elem, en, ru);
+                    result.Add(response.TranslatedText);
+                }
+                
+            }
+            return result;
+        }
+
+        public static ICollection<string> TranslateTextEnToRu(string[] couplets)
+        {
+            var result = new List<string>();
+            using (var client = TranslationClient.Create())
+            {
+                foreach (var elem in couplets)
+                {
+                    var response = client.TranslateText(elem, ru, en);
+                    result.Add(response.TranslatedText);
+                }
+
+            }
+            return result;
+        }
+
+    }
+}
--- berua-backend/testdbdb/Program.cs
+++ berua-backend/testdbdb/Program.cs
@@ -1,4 +1,5 @@
-﻿using berua.BLL.Actions;
+﻿using berua.BLL;
+using berua.BLL.Actions;
 using berua.BLL.DTO;
 using System;
 using System.IO;
@@ -9,16 +10,20 @@ namespace testdbdb
     {
         static void Main(string[] args)
         {
-            var user = new UserDTO
+            string[] gg = new string[]
             {
-                Login = "Жанна",
-                Password = "11122233",
-                Phone = "9217777777"
+                @"In a race that started at midnight local time, Briton Charlotte Purdue was among the athletes to pull out in temperatures of 32C and with humidity reaching over 70%.",
+                @"Organisers decided to go ahead with the event in its scheduled slot despite fears that the conditions might not be conducive for marathon running.",
+                @"Ethiopia's marathon coach Haji Adillo Roba witnessed his trio of athletes stop, including Tokyo Marathon winner Ruti Aga.",
+                @"We never would have run a marathon in these conditions in our own country, he told BBC Sport during the race.",
             };
+            var res = TranslateText.TranslateTextEnToRu(gg);
 
-            UserAction.RegistrationUser(user);
+            foreach(var i in res)
+            {
+                Console.WriteLine(i);
 
-            UserAction.ValidateUser(user.Login, user.Password);
+            }
 
         }
     }
