3678780c51a037d9581cf6e302741e43eb78b219
German Grachev <germangrach.94@yandex.ru>
2019-09-28 11:55:19 +0300

    Add translate

--- berua-backend/testdbdb/Program.cs
+++ berua-backend/testdbdb/Program.cs
@@ -1,4 +1,5 @@
-﻿using berua.BLL.Actions;
+﻿using berua.BLL;
+using berua.BLL.Actions;
 using berua.BLL.DTO;
 using System;
 using System.IO;
@@ -9,16 +10,24 @@ namespace testdbdb
     {
         static void Main(string[] args)
         {
-            var user = new UserDTO
+
+            string[] coll = new string[]
             {
-                Login = "Жанна",
-                Password = "11122233",
-                Phone = "9217777777"
+
+                @"The White House is reportedly considering de-listing Chinese companies from US stock exchanges as part of a broader effort to curb US investment in China.",
+                @"The US is also said to be looking at limiting the ability of government pension funds to participate in Chinese markets.",
+                @"The discussion comes amid a tense trade stand-off between the two countries.",
+                @"US stock exchanges dropped on the news, which Bloomberg first reported.",
+                @"Shares in Chinese companies listed in the US, which include big names such as Alibaba and JD.com, also fell sharply after the reports.",
+                @"Alibaba fell 5% while JD.com dropped more than 6 %."
             };
 
-            UserAction.RegistrationUser(user);
-
-            UserAction.ValidateUser(user.Login, user.Password);
+            var res = TranslateText.TranslateEnToRu(coll);
+            foreach(var el in res)
+            {
+                Console.WriteLine(el);
+            }
+            Console.ReadLine();
 
         }
     }
