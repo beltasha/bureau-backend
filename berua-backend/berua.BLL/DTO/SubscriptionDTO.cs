bababe4bdd689d524cf7ca95b614cb71c84b3832
German Grachev <germangrach.94@yandex.ru>
2019-09-28 20:12:05 +0300

    WIP

--- berua-backend/berua.BLL/DTO/SubscriptionDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace berua.BLL.DTO
-{
-    public class SubscriptionDTO
-    {
-        public int UserId { get; set; }
-        public string AccountKey { get; set; }
-        public SocialNetworkType Type { get; set; }
-    }
-}
